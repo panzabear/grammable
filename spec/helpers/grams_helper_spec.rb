bba69513fec120d91dff3206aa5cd537402b0525
Emilio Torres <emiliotorres75@gmail.com>
2019-08-30 19:50:48 +0000

    first rspec test

--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -11,5 +11,15 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe GramsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
+
+RSpec.describe GramsController, type: :controller do
+  describe "grams#index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
+
+
