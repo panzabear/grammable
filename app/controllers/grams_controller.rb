b0c521afc6c25bdb296c23b2f803ac5b59d7e8a7
Emilio Torres <emiliotorres75@gmail.com>
2019-09-01 23:24:02 +0000

    more tests

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,6 @@
 class GramsController < ApplicationController
 
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def new
         @gram = Gram.new
@@ -8,27 +8,40 @@ class GramsController < ApplicationController
 
     def edit
         @gram = Gram.find_by_id(params[:id])
-        if @gram.blank?
-            render plain: 'Not Found :(', status: :not_found
-        end
+        return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
     end
 
     def index
     end
+
+    def update
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
+
+        @gram.update_attributes(gram_params)
+
+        if @gram.valid?
+            redirect_to root_path
+        else
+            return render :edit, status: :unprocessable_entity
+        end
+    end
     
     def show
         @gram = Gram.find_by_id(params[:id])
-        if @gram.blank?
-            render plain: 'Not Found :(', status: :not_found
-        end
+        return render_not_found if @gram.blank?
     end
     
     def destroy
         @gram = Gram.find_by_id(params[:id])
-        return render plain: 'Not Found :(', status: :not_found if @gram.blank?
+        return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
         @gram.destroy
         redirect_to root_path
       end
+    end
     
 
 
@@ -46,6 +59,10 @@ class GramsController < ApplicationController
 
     def gram_params
         params.require(:gram).permit(:message)
+    end
+
+    def render_not_found
+        render plain: '#{status.to_s.titleize}:(', status: status
     end
 
 
